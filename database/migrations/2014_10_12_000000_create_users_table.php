709e12a82042d0dc8c78f69bb1bd56f00784d5f5
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-20 18:30:16 +0200

    Adjusted user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('email')->nullable();
             $table->string('password');
+            $table->unsignedInteger('identifier')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
