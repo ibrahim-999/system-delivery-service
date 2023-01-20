beb93d7da3271f373781d3c1c23c67c2c4dd582e
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-20 19:11:31 +0200

    Created user seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use App\Models\UserTemplate;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = UserTemplate::where("name", '=', 'admin')->first();
+
+        $this->rootUser($admin);
+
+    }
+
+
+    public function rootUser($template)
+    {
+        $rootUser                 = new User;
+        $rootUser->username       = "root";
+        $rootUser->email          = "himoath@gmail.com";
+        $rootUser->password       = Hash::make('123456');
+        $rootUser->remember_token = str_random(10);
+        $rootUser->template()->associate($template);
+        $rootUser->save();
+    }
+
+}
