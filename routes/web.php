6ae3dc26df10a1923552e827f7d3949546cc481d
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-24 13:16:58 +0200

    Added backend routes

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,79 @@
 |
 */
 
+use App\Models\Attachment;
+use App\Models\Client;
 use App\Models\Courier;
+use App\Models\Form;
+use App\Models\Inventory;
+use App\Models\Invoice;
+use App\Models\Note;
 use App\Models\Pickup;
+use App\Models\Service;
+use App\Models\Shipment;
+use App\Models\User;
+use App\Models\Zone;
 
 Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('tracking', "TrackController@show")->name('tracking.show');
 
-Courier::routes();
+Route::middleware('auth')->group(function () {
 
-Pickup::routes();
+    Shipment::routes();
+
+    Zone::routes();
+
+    User::routes();
+
+    Client::routes();
+
+    Attachment::routes();
+
+    Courier::routes();
+
+    Pickup::routes();
+
+    Service::routes();
+
+    Note::routes();
+
+    Invoice::routes();
+
+    Inventory::routes();
+
+    Form::routes();
+
+    Route::get('reports', "ReportingController@index")->name('reports.index');
+    Route::put('reports', "ReportingController@update")->name('reports.update');
+
+    Route::resource('settings', "SettingsController");
+    Route::put('settings', "SettingsController@update");
+
+    Route::post('branches', "BranchController@store")->name('branches.store');
+    Route::put('branches/{branch}', "BranchController@update")->name('branches.update');
+    Route::delete('branches/{branch}', "BranchController@destroy")->name('branches.destroy');
+
+    Route::get('emails', "MailingController@index")->name('emails.index');
+    Route::post('emails', "MailingController@broadcast")->name('emails.broadcast');
+
+    Route::prefix('ajax')->group(function () {
+        Route::get('notifications/all', 'NotificationsController@index');
+        Route::get('notifications/refresh', 'NotificationsController@refresh');
+        Route::get('notifications/clear', 'NotificationsController@clear');
+
+        Route::get('suggest/shipments', "Api\SuggestController@shipments");
+        Route::get('suggest/clients', "Api\SuggestController@clients");
+        Route::get('suggest/guest/{national_id}', "Api\SuggestController@guests");
+        Route::get('suggest/couriers', "Api\SuggestController@couriers");
+        Route::get('suggest/status/{status}', "Api\SuggestController@statuses");
+
+    });
+
+});
+
 
 
