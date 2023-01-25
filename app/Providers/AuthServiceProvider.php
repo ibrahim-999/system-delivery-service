ec54e0a220e978a75a190624dd510617120d7407
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-01-25 11:41:53 +0200

    Injected new polices

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,10 +4,20 @@ namespace App\Providers;
 
 use App\Models\Client;
 use App\Models\Courier;
-use App\Models\Shipment;
+use App\Models\Form;
+use App\Models\Note;
+use App\Models\Pickup;
+use App\Policies\FormPolicy;
+use App\Policies\PickupPolicy;
+use App\Policies\NotePolicy;
+use App\Policies\ServicePolicy;
+use App\Policies\ShipmentPolicy;
 use App\Policies\ClientPolicy;
 use App\Policies\CouriersPolicy;
-use App\Policies\ShipmentPolicy;
+use App\Policies\ZonePolicy;
+use App\Models\Service;
+use App\Models\Shipment;
+use App\Models\Zone;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -22,6 +32,11 @@ class AuthServiceProvider extends ServiceProvider
         Client::class => ClientPolicy::class,
         Courier::class => CouriersPolicy::class,
         Shipment::class => ShipmentPolicy::class,
+        Pickup::class => PickupPolicy::class,
+        Note::class => NotePolicy::class,
+        Zone::class => ZonePolicy::class,
+        Service::class => ServicePolicy::class,
+        Form::class => FormPolicy::class,
     ];
 
     /**
